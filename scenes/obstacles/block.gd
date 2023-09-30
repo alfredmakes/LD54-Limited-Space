e2fc671ea406ff68de2d98bed1dade3dfbfe8634
Alfred Clark <ajhc@posteo.net>
2023-09-30 18:54:52 +0100

    Blocks work mostly as expected now, YER DEAD

--- scenes/obstacles/block.gd
+++ scenes/obstacles/block.gd
@@ -1,10 +1,26 @@
 class_name Block
-extends RigidBody2D
+extends AnimatableBody2D
 
-@export var fall_speed: float = 0.5
+@export var fall_speed: float = 50
 @export var block_unit_width: int
 
 
-func _ready() -> void:
-	linear_velocity = Vector2(0, fall_speed)
 
+func _physics_process(delta: float) -> void:
+	var collision: KinematicCollision2D = KinematicCollision2D.new()
+	if test_move(transform, Vector2(0, fall_speed * delta), collision):
+		if collision.get_collider().is_in_group("Player"):
+			
+			var player = collision.get_collider() as CharacterBody2D
+			if player.is_on_floor():
+				print("yer dead")
+			else:
+				position += Vector2(0, fall_speed * delta)
+				return
+			
+		position.x = ceil(position.x)
+		position.y = ceil(position.y)
+		set_physics_process(false)
+		return
+	
+	position += Vector2(0, fall_speed * delta)
