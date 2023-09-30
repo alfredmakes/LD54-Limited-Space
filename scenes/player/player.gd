b52b07fd621cd5bf4c76c19df29b4053ea3c7b34
Alfred Clark <ajhc@posteo.net>
2023-09-30 20:59:33 +0100

    Gravityslide

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,6 +7,8 @@ const COYOTE_TIME = 0.1
 
 var time_in_air = 0.0
 
+var wall_slide_direction = 0.0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,7 +16,10 @@ var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if abs(wall_slide_direction) > 0.5:
+			velocity.y += gravity * delta * 0.4
+		else:
+			velocity.y += gravity * delta
 		time_in_air += delta
 	else:
 		time_in_air = 0
@@ -34,9 +39,15 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 #
-#	for i in get_slide_collision_count():
-#		var collision = get_slide_collision(i)
-#		print("Collided with: ", collision.get_collider().name)
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print("Collided with: ", collision.get_collider().name)
+		if abs(collision.get_normal().x) > 0.5:
+			wall_slide_direction = ceil(collision.get_normal().x)
+		else:
+			wall_slide_direction = 0.0
+		
+		print(wall_slide_direction)
 
 
 
